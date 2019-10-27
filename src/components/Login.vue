e71e5c6f420c18ad7574577c4b1872aa9e5229e3
Neelansh Mathur <neelansh15@outlook.com>
2019-10-27 14:24:00 +0530

    Some Bulma Form design

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,30 @@
 <template>
-    <div class="form">
-        <form>
-            <label for="username">Username:</label>
-            <input type="text" name="username">
-            <label for="password">Password:</label>
-            <input type="password" name="password">
-        </form>
+    <div class="container">
+        <div class="hero">
+            <div class="hero-body">
+                <form>
+                    <div class="field">
+                        <label class="label">Username</label>
+                        <div class="control">
+                            <input type="text" name="username" placeholder="Username">
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <label class="label">Password</label>
+                        <div class="control">
+                            <input type="text" name="password" placeholder="Password">
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <div class="control">
+                            <button class="button is-primary">Submit</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 </template>
 
